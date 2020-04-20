134804dacad84d03cca2b4a91d8991e50b18736f
Kerry Smith <kerrysfs@gmail.com>
2020-04-19 22:45:43 -0700

    addedjsreadandformatfixedbugnumber3

---
+++ README.md
@@ -0,0 +1,39 @@
+![Github avatar](https://avatars3.githubusercontent.com/u/59150488?v=4)
+
+## Username
+## Kerry-Jr
+# Title
+####  Unit 10 OOP Homework: Template Engine - Employee Summary
+# Table of contents:
+1. Description
+1. Installation
+1. Usage
+1. Test
+1. Contributors
+##### Description
+1.)One of the most important aspects of programming is writing code that is readable, reliable, and maintainable. Oftentimes, *how* we design our code is just as important as the code itself. In this homework assignment, your challenge is to build a Node CLI that takes in information about employees and generates an HTML webpage that displays summaries for each person. Since testing is a key piece in making code maintainable, you will also be ensuring that all unit tests pass.
+##### Installation
+2.)npm i
+##### Usage
+3.)generate a webpage that displays my team's basic info
+##### Test
+4.)JEST
+##### Contributors
+5.)Tutor
+##### Licenses
+6.)No license
+##### Email
+7.)[My Email. Please click me to send a message](mailto:kerrysfs@gmail.com)
+
+
+![Screen of working App](./assets/image/app.PNG)
+
+[Github repo link](https://github.com/Kerry-Jr "Your github repo")
+  
+
+  
+ 
+  
+
+  
+
--- app.js
+++ app.js
@@ -4,29 +4,21 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
 const render = require("./lib/htmlRenderer");
-​
-​//questions to ask
-
-//1.) Ask for name, ID, email, manager officenumber, intern school, engineer ask github username. 
 
 inquirer.prompt([
     {
         type: 'list',
-        message: 'Please pick your role',
+        message: 'What is your role?',
+        name: 'role',
         choices: [
             'Manager',
             'Intern',
             'Engineer'
         ]
     },
-    
-    
-    
     {
         type: 'input',
         message: 'What is your name?',
@@ -41,46 +33,64 @@ inquirer.prompt([
         type: 'input',
         message: 'What is your email?',
         name: 'email'
-    },
-    {
-        type: 'input',
-        message: 'What is your github username?',
-        name: 'github'
-    },
-    {
-        type: 'input',
-        message: 'Please enter the name of school',
-        name: 'school'
-    },
-    {
-        type: 'input',
-        message: 'What is the office number?',
-        name: 'officeNumber'
     }
-    
-]).then(data =>{
+]).then(async data => {
+    if (data.role === 'Manager') {
+        await inquirer.prompt([
+            {
+                type: 'input',
+                message: 'What is the office number?',
+                name: 'officeNumber'
+            }
 
-})
+        ]).then(function (response) {
+            const manager = new Manager(data.name, data.id, data.email, response.officeNumber);
+            roster.push(manager);
+        })
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+    } else if (data.role === 'Intern') {
+        await inquirer.prompt([
+            {
+                type: 'input',
+                message: 'Please enter the name of school',
+                name: 'school'
+            }
+        ]).then(function (response) {
+            const intern = new Intern(data.name, data.id, data.email, response.school);
+            roster.push(intern);
+
+        });
+
+    } else if (data.role === 'Engineer') {
+        await inquirer.prompt([
+            {
+                type: 'input',
+                message: 'What is your github username?',
+                name: 'github'
+            }
+        ]).then(function (response) {
+            const engineer = new Engineer(data.name, data.id, data.email, response.github);
+            roster.push(engineer);
+
+        })
+
+    }
+    fs.appendFile(outputPath, render(roster), err => {
+        if (err) {
+            console.log(err);
+            throw err
+        }
+        console.log('success');
+    })
+}
+);
+
+const roster = [];
+
+
+
+// render(roster);
+
+
+
+
